59d36bad74204c2fa3f11cc347133cc825da3b44
Benash <benashlutumba@gmail.com>
2026-02-15 15:51:13 +0100

    benash

---
+++ routes/web.php
@@ -0,0 +1,18 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+Route::get('/', function () {
+    return view('welcome');
+})->name('home');
+
+Route::view('dashboard', 'dashboard')
+    ->middleware(['auth', 'verified'])
+    ->name('dashboard');
+
+require __DIR__.'/settings.php';
+
+Route::get('/documents/create', function () {
+    return view('documents.create');
+})->middleware('auth');
+
